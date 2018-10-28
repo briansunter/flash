229645ca84e22c7ce46b5c3fc38f6be8865cd801
Brian Sunter <bsunter@gmail.com>
2018-10-27 18:10:22 -0700

    Basic Graphql

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,7 +8,10 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "us-east-1:eaed4d9c-a295-470f-aeac-5fd8338a7326",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_XE5OiTGa2",
-    "aws_user_pools_web_client_id": "7ve8cfk17jm625m6vt4ct4422n"
+    "aws_user_pools_web_client_id": "7ve8cfk17jm625m6vt4ct4422n",
+    "aws_appsync_graphqlEndpoint": "https://sitl242mkzdsfmggkithcg442y.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
